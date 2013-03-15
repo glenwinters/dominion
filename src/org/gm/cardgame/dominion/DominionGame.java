5d361c620c28d165d2ebe35055a12d1b6b31cdd9
Mike P <mwpodiluk@gmail.com>
2013-03-15 11:25:56 -0600

    Missed formatting one file earlier. No code change.

--- src/org/gm/cardgame/dominion/DominionGame.java
+++ src/org/gm/cardgame/dominion/DominionGame.java
@@ -8,26 +8,29 @@ import org.gm.cardgame.dominion.cards.DominionCard;
 
 public class DominionGame extends Game
 {
-	protected final List<DominionPlayer> players;
-	protected final DominionTable table;
-    public DominionGame(List<DominionPlayer> players, List<DominionCard> cards)
+    protected final List<DominionPlayer> players;
+    protected final DominionTable table;
+
+    public DominionGame( List<DominionPlayer> players, List<DominionCard> cards )
     {
         this.players = players;
-        this.table = new DominionTable(cards);
+        this.table = new DominionTable( cards );
     }
-    
+
     public boolean checkGameEnd()
     {
         return table.checkGameEnd();
     }
 
-	@Override
-	public void startGame(List<Player> players) {
-		
-	}
+    @Override
+    public void startGame( List<Player> players )
+    {
 
-	@Override
-	public void nextTurn() {
-		
-	}
+    }
+
+    @Override
+    public void nextTurn()
+    {
+
+    }
 }
