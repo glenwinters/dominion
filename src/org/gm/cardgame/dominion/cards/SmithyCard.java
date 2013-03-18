3730a345b23ffe74711df4778838c54034f831f2
Glen Winters <lamouche@gmail.com>
2013-03-19 00:30:59 +0100

    Added SmithyCard

---
+++ src/org/gm/cardgame/dominion/cards/SmithyCard.java
@@ -0,0 +1,23 @@
+package org.gm.cardgame.dominion.cards;
+
+import java.util.EnumSet;
+
+import org.gm.cardgame.dominion.DominionGame;
+import org.gm.cardgame.dominion.DominionPlayer;
+
+public class SmithyCard extends DominionCard
+{
+    public SmithyCard()
+    {
+        super( "Smithy", 4, 0, EnumSet.of( DominionCard.CardType.ACTION ),
+                DominionCard.CardSet.BASE );
+    }
+
+    @Override
+    public void onPlay( DominionGame game )
+    {
+        DominionPlayer currentPlayer = game.getCurrentPlayer();
+
+        currentPlayer.drawCards( 3 );
+    }
+}
