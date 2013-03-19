e1906a17d07b3e544fdaafbd77d36c091203b7e4
Glen Winters <lamouche@gmail.com>
2013-03-19 04:09:49 +0100

    Added CouncilRoomCard

---
+++ src/org/gm/cardgame/dominion/cards/CouncilRoomCard.java
@@ -0,0 +1,31 @@
+package org.gm.cardgame.dominion.cards;
+
+import java.util.EnumSet;
+import java.util.List;
+
+import org.gm.cardgame.dominion.DominionGame;
+import org.gm.cardgame.dominion.DominionPlayer;
+
+public class CouncilRoomCard extends DominionCard
+{
+    public CouncilRoomCard()
+    {
+        super( "Council Room", 5, 0, EnumSet.of( DominionCard.CardType.ACTION ),
+                DominionCard.CardSet.BASE );
+    }
+
+    @Override
+    public void onPlay( DominionGame game )
+    {
+        DominionPlayer currentPlayer = game.getCurrentPlayer();
+
+        game.addBuys( 1 );
+
+        currentPlayer.drawCards( 4 );
+
+        for ( DominionPlayer opponent : game.getOpponents() )
+        {
+            opponent.drawCards( 1 );
+        }
+    }
+}
