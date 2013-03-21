3b4cd59792716fa0140e0b4f0aa8524e7b8433c0
Glen Winters <lamouche@gmail.com>
2013-03-21 23:45:13 +0100

    Added WitchCard

---
+++ src/org/gm/cardgame/dominion/cards/base/WitchCard.java
@@ -0,0 +1,30 @@
+package org.gm.cardgame.dominion.cards.base;
+
+import java.util.EnumSet;
+import java.util.List;
+
+import org.gm.cardgame.dominion.DominionGame;
+import org.gm.cardgame.dominion.DominionPlayer;
+import org.gm.cardgame.dominion.cards.DominionCard;
+
+public class WitchCard extends DominionCard
+{
+    public WitchCard()
+    {
+        super( "Witch", 5, 0, EnumSet.of( DominionCard.CardType.ACTION, DominionCard.CardType.ATTACK ),
+                DominionCard.CardSet.BASE );
+    }
+
+    @Override
+    public void onPlay( DominionGame game )
+    {
+        DominionPlayer currentPlayer = game.getCurrentPlayer();
+
+        currentPlayer.drawCards( 2 );
+        
+        for (DominionPlayer opponent : game.getOpponents())
+        {
+            opponent.gainCard( game.gainCard( "Curse" ) );
+        }
+    }
+}
