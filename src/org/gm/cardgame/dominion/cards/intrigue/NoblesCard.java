3c2f21d008d63b22268419fa38a128b37a170149
Glen Winters <lamouche@gmail.com>
2013-04-14 12:52:20 +0200

    Added NoblesCard

---
+++ src/org/gm/cardgame/dominion/cards/intrigue/NoblesCard.java
@@ -0,0 +1,57 @@
+package org.gm.cardgame.dominion.cards.intrigue;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.List;
+
+import org.gm.cardgame.dominion.Deck;
+import org.gm.cardgame.dominion.DominionGame;
+import org.gm.cardgame.dominion.DominionPlayer;
+import org.gm.cardgame.dominion.cards.DominionCard;
+import org.gm.cardgame.dominion.cards.prosperity.WatchtowerCard.Choice;
+
+public class NoblesCard extends DominionCard
+{
+    private enum Choice
+    {
+        NONE( -1 ), DRAW3( 0 ), ACTIONS2( 1 );
+
+        private final int value;
+
+        Choice( int value )
+        {
+            this.value = value;
+        }
+
+        public int getValue()
+        {
+            return this.value;
+        }
+    };
+    public NoblesCard()
+    {
+        super( "Nobles", 6, 0, EnumSet.of( DominionCard.CardType.ACTION, DominionCard.CardType.VICTORY ), DominionCard.CardSet.INTRIGUE );
+    }
+
+    @Override
+    public void onPlay( DominionGame game )
+    {
+        DominionPlayer currentPlayer = game.getCurrentPlayer();
+        List<String> choices = Arrays.asList( "+3 Cards", "+2 Actions" );
+        int choice = currentPlayer.promptMultipleChoice( choices, true );
+        if( choice == Choice.DRAW3.getValue() )
+        {
+            game.getCurrentPlayer().drawCards( 3 );
+        }
+        else if( choice == Choice.ACTIONS2.getValue() )
+        {
+            game.addActions( 2 );
+        }
+    }
+
+    @Override
+    public int getVictoryPoints( Deck deck )
+    {
+        return 2;
+    }
+}
